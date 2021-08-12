17c9da1b4d3749e5e27b1ffb42abf06a4c11fcca
deboracaires <debora.caires.mor@gmail.com>
2021-08-12 15:17:24 -0300

    ao selecionar um quizz, o usuário é direcionado para a pagina do quizz onde apenas as respostas ainda não sao renderizadas

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ function renderizarQuizzes(){
     quizzes.innerHTML = '';
     for(let i=0; i<todosQuizzes.length; i++){
         quizzes.innerHTML += `
-        <li>
+        <li onclick="abrirQuizz(this, '${todosQuizzes[i].title}')">
             <div class="degrade">
                 <span>${todosQuizzes[i].title}</span>
             </div>
@@ -28,5 +28,43 @@ function renderizarQuizzes(){
     for(let i=0; i<todosQuizzes.length; i++){
         quiz.style.backgroundImage = `url("${todosQuizzes[i].image}")`;
         quiz = quiz.nextElementSibling;
+    }
+}
+let quizzEspecifico = {};
+// abre a paginaQuizz com o quizz selecionado respectivo
+function abrirQuizz(elemento, tituloQuizz){
+    const listaQuizzes = document.querySelector(".lista-quizzes");
+    listaQuizzes.classList.add("escondido");
+    const paginaQuizz = document.querySelector(".pagina-quizz");
+    paginaQuizz.classList.remove("escondido");
+    
+    for(let i=0; i<todosQuizzes.length; i++){
+        if(tituloQuizz === todosQuizzes[i].title){
+            quizzEspecifico = todosQuizzes[i];
+        }
+    } 
+    
+    renderizarQuizz();
+   
+}
+function renderizarQuizz(){
+    const mudaTopo = document.querySelector(".pagina-quizz .topo-quizz");
+   
+    mudaTopo.style.backgroundImage = `url("${quizzEspecifico.image}")`;
+    mudaTopo.innerHTML = `${quizzEspecifico.title}`;
+   
+    const inserePerguntas = document.querySelector(".pagina-quizz ul");
+    inserePerguntas.innerHTML = ``;
+    for(let i=0; i < quizzEspecifico.questions.length; i++){
+        const pergunta = quizzEspecifico.questions[i];
+        inserePerguntas.innerHTML += `
+            <li class="pergunta-quizz-conteiner">
+                <div class="pergunta-quizz azul">
+                    <strong> ${pergunta.title}</strong>
+                </div>
+                <div class="respostas-conteiner">
+                </div>
+            </li> 
+        `;
     }
 }
